d7acdbbcb942b6ca5447101b9efb6620d5df2df1
Jay Mehta <78312356+BitsJayMehta173@users.noreply.github.com>
2021-05-23 16:53:05 +0545

    Update ProjectEuler4.cpp

--- ProjectEuler4.cpp
+++ ProjectEuler4.cpp
@@ -1,3 +1,23 @@
+/*
+The palindrome can be written as:
+
+abccba
+
+Which then simpifies to:
+
+100000a + 10000b + 1000c + 100c + 10b + a
+
+And then:
+
+100001a + 10010b + 1100c
+
+Factoring out 11, you get:
+
+11(9091a + 910b + 100c)
+
+So the palindrome must be divisible by 11.  Seeing as 11 is prime, at least one of the numbers must be divisible by 11.  So brute force in Python, only with less numbers to be checked:
+
+*/
 //Jay Mehta
 #include <bits/stdc++.h>
 #define ll long long int
